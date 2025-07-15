81f2dfc86bd9b632f75d638b7f283557fdf3608f
Rian Marlon <rianmarlon20@yahoo.com>
2025-07-14 22:35:22 -0300

    feat: add transaction created kafka handler

---
+++ internal/events/handler/transaction_created_kafka.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"fmt"
+	"walletcore/pkg/events"
+	"walletcore/pkg/kafka"
+)
+
+type TransactionCreatedKafkaHandler struct {
+	Kafka *kafka.Producer
+}
+
+func NewTransactionCreatedKafkaHandler(kafka *kafka.Producer) *TransactionCreatedKafkaHandler {
+	return &TransactionCreatedKafkaHandler{
+		Kafka: kafka,
+	}
+}
+
+func (h *TransactionCreatedKafkaHandler) Handle(message events.EventInterface) {
+	h.Kafka.Publish(message, nil, "transactions")
+	fmt.Println("TransactionCreatedKafkaHandler: ", message)
+}
